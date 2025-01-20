b15f577ac5d2ea10eb024c9a82225c384810420e
elanofb <elanofb@gmail.com>
2025-01-20 18:45:49 -0300

    chore: salvando projeto antes de iniciar o Git Flow

--- TakingElano.IntegrationTests/obj/Debug/net9.0/TakingElano.IntegrationTests.AssemblyInfo.cs
+++ TakingElano.IntegrationTests/obj/Debug/net9.0/TakingElano.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakingElano.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13378bb2eb54ce3d096ea6f28e2b0f6f4966dc30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867b7af40740ad63eb18a4b6be5f60d3248182e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakingElano.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakingElano.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
